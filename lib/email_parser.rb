01ec3742a91ef1c7492cb11865d6bd06c798a140
Brent Bauman <brentbauman86@gmail.com>
2018-05-08 13:35:20 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,12 +9,12 @@ attr_accessor :emails
 
 
 def initialize(email)
-@@emails = emails
+@emails = emails
 end
 
 def parse
 email_array = []
-email_array = @@emails.split(", ")
+email_array = @emails.split(", ")
 email_array.uniq
 end
 
