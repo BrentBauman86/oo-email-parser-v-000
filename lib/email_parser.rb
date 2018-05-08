0548c9585d674c2f56e5fdc0247e5fb6e2b6e134
Brent Bauman <brentbauman86@gmail.com>
2018-05-08 15:15:59 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,8 +14,8 @@ end
 
 def parse
 email_array = emails.split(" ")
-email_array = email_array.collect do |email|
-  emails.split(", ") 
+email_array.collect do |email|
+  emails.split(", ")
 end
 email_array.uniq
 end
